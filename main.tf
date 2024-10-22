8858d84ecef9f53d325f5438e8f339bb77260040
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-10-22 07:59:22 -0700

    Call user-data for do droplet resource

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# TODO: Remove provider and put in the docs so it is documented for calling
+# this module
 provider "digitalocean" {
   token             = var.do_token
   spaces_access_id  = var.spaces_access_id
@@ -14,13 +16,13 @@ resource "digitalocean_floating_ip_assignment" "miab" {
 }
 
 resource "digitalocean_ssh_key" "miab" {
-  name       = "SSH key for mail in a box at ${var.fqdn}"
+  name       = "SSH key for mail in a box at ${var.miab_primary_hostname}"
   public_key = file("${var.ssh_private_key}.pub")
 }
 
 # Bucket for MIAB/Nextcloud FUSE mount
 resource "digitalocean_spaces_bucket" "miab" {
-  name   = var.fqdn
+  name   = var.miab_primary_hostname
   region = var.do_region
   count  = var.spaces_backup_enabled == "" ? 1 : 0
 
@@ -37,23 +39,25 @@ resource "digitalocean_spaces_bucket" "miab" {
   # }
 }
 
-resource "digitalocean_droplet" "miab" {
-  image    = var.droplet_image
-  name     = var.fqdn
-  ipv6     = true
-  region   = var.do_region
-  size     = var.droplet_size
-  ssh_keys = [digitalocean_ssh_key.miab.fingerprint]
-  backups  = true
-
-  depends_on = [digitalocean_spaces_bucket.miab]
-
-  user_data = templatefile("${path.module}/miab-setup.yaml", {
-    ssh_port = var.ssh_port
-  })
+resource "random_integer" "ssh_port" {
+  min = 1025
+  max = 65535
 }
+resource "digitalocean_droplet" "miab" {
+  backups    = true
+  depends_on = [digitalocean_spaces_bucket.miab]
+  image      = var.droplet_image
+  ipv6       = false
+  name       = var.miab_primary_hostname
+  region     = var.do_region
+  size       = var.droplet_size
+  ssh_keys   = [digitalocean_ssh_key.miab.fingerprint]
 
-output "floating_ip_address" {
-  value     = digitalocean_floating_ip.miab.ip_address
-  sensitive = true
+  user_data = templatefile("${path.module}/miab-setup.tftpl", {
+    miab_primary_hostname = var.miab_primary_hostname
+    miab_public_ip        = digitalocean_floating_ip.miab.ip_address
+    miab_storage_root     = var.miab_storage_root
+    miab_storage_user     = var.miab_storage_user
+    ssh_port              = random_integer.ssh_port.result
+  })
 }
